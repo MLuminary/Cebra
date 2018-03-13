42dad1a01ea195067736bc8fefb0b771b045ab5b
LBinin <linbin@lbinin.com>
2018-03-13 18:27:26 +0800

    添加头部组件

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
   <div>
+    <Header/>
     <router-view class="view"></router-view>
     <tab/>
   </div>
 </template>
 
 <script>
-import Tab from 'components/tab'
+import Tab from 'components/tab/tab'
+import Header from 'components/header/header'
 
 export default {
   name: 'App',
   components: {
-    Tab
+    Tab,
+    Header
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-@import '~common/stylus/variable'
+@import '~common/stylus/variable';
 
 .view {
-  position absolute
-  top 0
-  left 0
-  right 0
-  bottom $TAB_HEIGHT
-  overflow hidden
+  position: absolute;
+  top: $HEADER_HEIGHT;
+  left: 0;
+  right: 0;
+  bottom: $TAB_HEIGHT;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus" scoped>
+@import '~common/stylus/variable'
+
+.header {
+  height: $HEADER_HEIGHT;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  position absolute
+  left 0
+  right 0
+  top 0
+}
+</style>
