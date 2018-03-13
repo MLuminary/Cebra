f8de01a90aab30112bb9b474d694cb9c1fa0c5c0
LBinin <linbin@lbinin.com>
2018-03-14 01:13:18 +0800

    添加样式 reset、base

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
 import ElementUI from 'element-ui'
+import 'common/stylus/index.styl'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.develtools = true
